cfdd68803f27e382770d8189b8d01c9101ad36d1
kazirshahria <kazirshahria@gmail.com>
2025-06-15 02:03:12 -0400

    Changed location and usage of Client functions

--- src/api/stonex/client.py
+++ src/api/stonex/client.py
@@ -1,8 +1,21 @@
 import os
 from .utils.stonex_utils import send_request
 
-class Account(object):
+class Client():
+    """
+    # Forex.com Client
     
+    A class that verifys and stores client information with Forex.com.
+    
+    Information includes:
+        - Number of accounts with the broker
+        - Balance
+        - Margin requirements
+        - and much more!
+
+    Args:
+        
+    """
     def __init__(self, username: str=os.environ.get('USERNAME'), password: str=os.environ.get('PASSWORD'), app_key: str=os.environ.get('APP_KEY')):
         self.username: str = username
         self.password: str = password
@@ -10,10 +23,18 @@ class Account(object):
 
         self.url: str = os.environ.get('HOST')
         self.accounts: list = None
-        self.balance: dict = None
+        self.balance_data: dict = None
+        self.cash: float = None
+        self.margin: float = None
+        self.margin_requirements: float = None
+        self.equity: float = None
+        self.pnl: float = None
 
         self.session_id: str = None
         self.client_id: str = None
+
+    def __repr__(self):
+        return f'Account(username={self.username}, password={self.password}, app_key={self.app_key})'
 
     def open_new_session(self):
         response_code, response = send_request(
@@ -27,10 +48,12 @@ class Account(object):
                 'AppKey': self.app_key
             }
         )
-
+        
         if (response_code == 200) & (self.session_id == None):
             session = response.get("session")
             self.session_id = session
+            self.locate_trading_accounts()
+            self.update_account_balance()
             return session
 
     def close_existing_session(self):
@@ -49,8 +72,8 @@ class Account(object):
         if (response_code == 200) & (response.get("LoggedOut") == True):
             print(f'Session closed')
             exit()
-
-    def client_trading_accounts(self):
+    
+    def locate_trading_accounts(self):
         response_code, response = send_request(
             method='GET',
             url=self.url,
@@ -67,11 +90,12 @@ class Account(object):
             self.accounts = trading_accounts
 
             client_account_id = trading_accounts[0].get('clientAccountId')
-            self.client_account_id = client_account_id
-
-            return trading_accounts, client_account_id
-    
-    def balance_information(self) -> dict:
+            self.client_id = client_account_id
+            return print(
+                f'Number of accounts: {len(trading_accounts)}'
+            )
+        
+    def update_account_balance(self):
         response_code, response = send_request(
             method='GET',
             url=self.url,
@@ -80,10 +104,18 @@ class Account(object):
             {
                 'UserName': self.username,
                 "Session": self.session_id,
-                'ClientAccountId': self.client_account_id
+                'ClientAccountId': self.client_id
             }
         )
 
         if response_code == 200:
-            self.balance = response
-            return response
+            self.balance_data = response
+            self.cash = response.get('cash')
+            self.margin = response.get('margin')
+            self.margin_requirements = response.get('totalMarginRequirement')
+            self.equity = response.get('netEquity')
+            self.pnl = response.get('openTradeEquity')
+            return print(
+                f'Cash: ${self.cash} \nMargin: ${self.margin} \nMargin Requirements: ${self.margin_requirements}\
+                \nEquity: ${self.equity} \nP/L: ${self.pnl}'
+            )
