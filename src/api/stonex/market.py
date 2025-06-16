8112587fa9d4acde63a6fae783e90a29b0c1d932
kazirshahria <kazirshahria@gmail.com>
2025-06-16 15:48:08 -0400

    Get the market tags for a client and save it into a csv file

--- src/api/stonex/market.py
+++ src/api/stonex/market.py
@@ -1,4 +1,31 @@
+import pandas as pd
+from .client import Client
+from .utils.stonex_utils import send_request
 
-def market():
-    print("Market initalized")
+class Market():
+    """
     
+    """
+    def __init__(self, client: Client):
+        self.client: Client = client
+    
+    def available_market_tags(self, file_name: str='avaliable_markets.csv'):
+        """
+        The market tags available in a client's account
+        """
+        response_code, response = send_request(
+            method='GET',
+            url=self.client.url,
+            path='/v2/market/tagLookup',
+            params=\
+            {
+                'UserName': self.client.username,
+                'Session': self.client.session_id,
+                'ClientAccountId': self.client.client_id
+            }
+        )
+        if response_code == 200:
+            markets = response['tags'][0]['children']
+            df = pd.DataFrame(markets).set_index('marketTagId')
+            df.to_csv(f'./data/{file_name}')
+            return df
