538b6f329741b60a7585b27bc029274cdf317584
kazirshahria <kazirshahria@gmail.com>
2025-06-17 00:50:57 -0400

    Created a template for the instrument class

--- src/api/stonex/instrument.py
+++ src/api/stonex/instrument.py
@@ -1,3 +1,16 @@
+from .client import Client
+from .utils.stonex_utils import send_request
 
-def instrument():
-    print("Instrument initalized")
+class Instrument():
+    
+    def __init__(self, client: Client, market_id: str, name: str, margin: float, min_margin: float, max_margin: float, client_margin: float, bid_price: float, ask_price: float, spread: float):
+        self.client = client
+        self.market_id = market_id
+        self.name = name
+        self.margin = margin
+        self.min_margin = min_margin
+        self.max_margin = max_margin
+        self.client_margin = client_margin
+        self.bid_price = bid_price
+        self.ask_price = ask_price
+        self.spread = spread
