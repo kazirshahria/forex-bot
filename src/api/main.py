d815113d9149e4f140207eda742243799dcc003d
kazirshahria <kazirshahria@gmail.com>
2025-06-17 00:48:27 -0400

    Fixed the file to properly close a session when performing tests

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,15 @@
 from stonex.client import Client
+from stonex.market import Market
 
-acc = Client()
-print(acc)
-acc.open_new_session()
-acc.close_existing_session()
+account = Client()
+try:
+    account.open_new_session()
+    market = Market(account)
+    pairs = market.search_currency_pairs()
+    print(pairs)
+except Exception as e:
+    print(f"Exception: {e}")
+    pass
+finally:
+    account.close_existing_session()
+
