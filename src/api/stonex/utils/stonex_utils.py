a92134b15142e898820c72584d122adae5dcc58a
kazirshahria <kazirshahria@gmail.com>
2025-06-19 22:41:42 -0400

    Added a function to log messages onto the terminal window

--- src/api/stonex/utils/stonex_utils.py
+++ src/api/stonex/utils/stonex_utils.py
@@ -1,6 +1,8 @@
 import requests
+from datetime import datetime
 
-def send_request(method: str, url: str, path: str, json: dict=None, params: dict=None):
+
+def send_request(method: str, url: str, path: str, json: dict = None, params: dict = None):
     url = "https://" + url + path
     response = requests.request(
         method=method,
@@ -10,9 +12,12 @@ def send_request(method: str, url: str, path: str, json: dict=None, params: dict
         timeout=120
     )
     response_code = response.status_code
-    
+
     if response_code == 200:
         return response_code, response.json()
     else:
         return response_code, None
-    
+
+
+def log(level: str, event: str, msg: str):
+    return print(f'[{datetime.now().isoformat(sep=" ", timespec="seconds")}] - [{level}] [{event}] >>> {msg}')
