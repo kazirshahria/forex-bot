e869851ea607efb734d566446f17049bbeab3ecb
kazirshahria <kazirshahria@gmail.com>
2025-06-19 22:46:52 -0400

    Minor changes to the fetching markets function

--- src/api/stonex/market.py
+++ src/api/stonex/market.py
@@ -3,24 +3,21 @@ from .client import Client
 from .instrument import Instrument
 from .utils.stonex_utils import send_request
 
-class Market():
-    """
-    
-    """
+
+class Market(object):
+
     def __init__(self, client: Client):
         self.client: Client = client
         self.url = client.url
-    
-    def available_market_tags(self, file_name: str='avaliable_markets.csv'):
-        """
-        Avaliable markets and unique identifiers for a client to trade in
+
+    def export_markets(self, file_name: str):
+        """Available markets and unique identifiers for a client to trade in
         """
         response_code, response = send_request(
             method='GET',
             url=self.client.url,
             path='/v2/market/tagLookup',
-            params=\
-            {
+            params={
                 'UserName': self.client.username,
                 'Session': self.client.session_id,
                 'ClientAccountId': self.client.client_id
@@ -32,15 +29,29 @@ class Market():
             df.to_csv(f'./data/{file_name}')
             return df
 
-    def search_currency_pairs(self, query: str='USD', market_tag: str='81'):
+    def market_information(self, market_id: str):
+        response_code, response = send_request(
+            method='GET',
+            url=self.url,
+            path=f'/v2/market/{market_id}/information',
+            params={
+                'UserName': self.client.username,
+                'Session': self.client.session_id,
+                'ClientAccountId': self.client.client_id
+            }
+        )
+
+        if response_code == 200:
+            print(response)
+
+    def currency_pairs(self, currency: str = 'USD', market: str = '81'):
         response_code, response = send_request(
             method='GET',
             url=self.url,
             path='/v2/market/fullSearchWithTags',
-            params=\
-            {
-                'tagId': market_tag,
-                'query': query,
+            params={
+                'tagId': market,
+                'query': currency,
                 'maxResults': 200,
 
                 'UserName': self.client.username,
@@ -51,16 +62,16 @@ class Market():
 
         if response_code == 200:
             currency_pair_dict = {}
-            market_informations = response['marketInformation']
-            for market_information in market_informations:
-                market_id = market_information.get('marketId')
-                name = market_information.get('name')
-                margin = market_information.get('marginFactor')
-                min_margin = market_information.get('minMarginFactor')
-                max_margin = market_information.get('maxMarginFactor')
-                client_margin = market_information.get('clientMarginFactor')
+            market_information = response['marketInformation']
+            for information in market_information:
+                market_id = information.get('marketId')
+                name = information.get('name')
+                margin = information.get('marginFactor')
+                min_margin = information.get('minMarginFactor')
+                max_margin = information.get('maxMarginFactor')
+                client_margin = information.get('clientMarginFactor')
                 
-                prices = market_information.get('prices')
+                prices = information.get('prices')
                 bid_price = prices.get('bidPrice')
                 ask_price = prices.get('offerPrice')
                 spread = ask_price - bid_price
@@ -68,7 +79,7 @@ class Market():
                 currency_pair_dict[name] = Instrument(
                     client=self.client,
                     market_id=market_id,
-                    name = name, 
+                    name=name,
                     margin=margin,
                     min_margin=min_margin,
                     max_margin=max_margin,
@@ -78,20 +89,3 @@ class Market():
                     spread=spread
                 )
         return currency_pair_dict
-
-    def market_information(self, market_id: str):
-        response_code, response = send_request(
-            method='GET',
-            url=self.url,
-            path=f'/v2/market/{market_id}/information',
-            params=\
-            {
-                'UserName': self.client.username,
-                'Session': self.client.session_id,
-                'ClientAccountId': self.client.client_id
-            }
-        )
-        
-        if response_code == 200:
-            print(response)
-
